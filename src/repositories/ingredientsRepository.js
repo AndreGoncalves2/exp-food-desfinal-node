a7e493337018907487cc2a0c076898df5da0bd80
André Gonçalves <andrevilanovagon@hotmail.com>
2023-08-23 20:55:18 -0300

    save

--- src/repositories/ingredientsRepository.js
+++ src/repositories/ingredientsRepository.js
@@ -1,10 +1,15 @@
 const knex = require("../database/knex");
 
-class IngredientsController {
+class IngredientsRepository {
     async create({ ingredientsInsert }) {
         const ingredients = await knex("ingredients").insert(ingredientsInsert);
         return ingredients;
     };
+
+    async findByDishId({ dish_id }) {
+        const ingredients= await knex("ingredients").where({ dish_id });
+        return ingredients;
+    }
 };
 
-module.exports = IngredientsController;
+module.exports = IngredientsRepository;
