b0808fd60c4d28e813947cf20a9b723bf93b6c6d
Andre <andrevilanovagon@hotmail.com>
2023-09-11 19:03:48 -0300

    save

--- src/repositories/saleRepository.js
+++ src/repositories/saleRepository.js
@@ -8,13 +8,14 @@ class SaleRepository {
 
     async getSale({ user_id }) {
         const sale = await knex("sale").join("order", "sale.id", "=", "order.sale_id")
-        .join("dish", "order.dish_id", "=", "dish.id").where("sale.user_id", user_id)
+        .join("dish", "order.dish_id", "=", "dish.id").select("sale.id AS sale_id", "dish.*", "order.dish_id", knex.raw('(SELECT GROUP_CONCAT(name, ", ") FROM dish INNER JOIN sale id = sale.dish_id) AS ingredient') ).where("sale.user_id", user_id);
         
-            
+        // .select('dish.*', knex.raw('(SELECT GROUP_CONCAT(, ", ") FROM ingredient WHERE dish_id = dish.id) AS ingredient')).select('dish.*', knex.raw('(SELECT GROUP_CONCAT(name, ", ") FROM ingredient WHERE dish_id = dish.id) AS ingredient')).select('dish.*', knex.raw('(SELECT GROUP_CONCAT(name, ", ") FROM ingredient WHERE dish_id = dish.id) AS ingredient')).select('dish.*', knex.raw('(SELECT GROUP_CONCAT(name, ", ") FROM ingredient WHERE dish_id = dish.id) AS ingredient')).select('dish.*', knex.raw('(SELECT GROUP_CONCAT(name, ", ") FROM ingredient WHERE dish_id = dish.id) AS ingredient')).select('dish.*', knex.raw('(SELECT GROUP_CONCAT(name, ", ") FROM ingredient WHERE dish_id = dish.id) AS ingredient')).select('dish.*', knex.raw('(SELECT GROUP_CONCAT(name, ", ") FROM ingredient WHERE dish_id = dish.id) AS ingredient')).select('dish.*', knex.raw('(SELECT GROUP_CONCAT(name, ", ") FROM ingredient WHERE dish_id = dish.id) AS ingredient'))
+
+
         console.log(sale);
         return sale;
-    }
-
+    };
 };
 
 module.exports = SaleRepository;
